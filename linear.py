2175d1a6a99847b2fcc46b9464976a36dff2c840
j225yu@uwaterloo.ca <j225yu@uwaterloo.ca>
2016-11-12 10:37:40 -0500

    residual plots complete

--- linear.py
+++ linear.py
@@ -19,6 +19,7 @@ regr = linear_model.LinearRegression()
 regr.fit(data,CV)
 predicted_results = regr.predict(data)
 
+plt.figure(1)
 plt.plot(data, predicted_results, color = 'green', linewidth =3)
 plt.scatter(data, CV, color='black')
 plt.title('Mental health as a function of song dance level')
@@ -40,3 +41,40 @@ print("RMSE =", RMSE)
 print("R2 =", R2)
 print("Mean residual sum of squares = %.2f" % np.mean((regr.predict(data) - CV) **2))
 print('R2 = %.2f' % regr.score(data, CV))
+
+residual_error = CV - predicted_results
+print("Mean of residuals =", np.std(residual_error))
+print("standard deviation of residuals =", np.std(residual_error))
+
+plt.figure(2)
+plt.plot((0,1),(0,0), 'r--')
+plt.scatter(data, residual_error, label='residual error')
+plt.title("Residual plot")
+plt.xlabel("dance")
+plt.ylabel("residual error")
+plt.show()
+
+plt.figure(3)
+plt.hist(residual_error)
+plt.title("Distribution of residuals")
+plt.title("residual error")
+plt.show()
+
+plt.figure(4)
+n, bins, patches = plt.hist(residual_error, 10, normed=1, alpha = 0.5)
+y_pdf = P.normpdf(bins, np.mean(residual_error), np.std(residual_error))
+l = P.plot(bins, y_pdf, 'k--', linewidth=1.5)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
